d6c865e01ed0b513e279e99fa3d82baaafbce7d4
Jens Villard <jensvillard@live.de>
2023-07-19 13:30:06 +0200

    s

---
+++ nanoFramework.WebServer/Extensions.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+
+namespace nanoFramework.WebServer
+{
+    public static class Extensions
+    {
+        public static string? TryGetValue(this UrlParameter[] urlParameters, string name)
+        {
+            foreach (var urlParameter in urlParameters)
+                if (urlParameter.Name == name) return urlParameter.Value;
+            return null;
+        }
+    }
+}
---
+++ nanoFramework.WebServer/Results/BadRequestResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace nanoFramework.WebServer.Results
+{
+    public class BadRequestResult : BaseResult
+    {
+        public BadRequestResult(string content) : base(content)
+        {            
+        }
+
+        public override int StatusCode => 400;
+    }
+}
